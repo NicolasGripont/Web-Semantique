4008887105256abfde6b9919d8cad1a5824cf0f7
NicolasGripont <Nicolas.Gripont@insa-lyon.fr>
2016-11-30 16:11:35 +0100

    Ajout Google Custom Search for php

---
+++ Sources/php/controleurs/controleur.php
@@ -0,0 +1,8 @@
+<?php
+require_once '../google-api-php-client-2.1.0/vendor/autoload.php';
+
+//identifiant du moteur de recher : 016014982774890444637:tirltd59_os
+//API key: AIzaSyAQ1rgsSDdetI6uhC9egwf_OqdDprHwB-g
+//		   AIzaSyBQhROXNGtRsvRaDvDQeax-Q5S33-U2yKQ	
+curl("https://www.googleapis.com/customsearch/v1?key=AIzaSyAQ1rgsSDdetI6uhC9egwf_OqdDprHwB-g&cx=016014982774890444637:tirltd59_os&q=vin");
+?>
