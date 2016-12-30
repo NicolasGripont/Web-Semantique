0c21e97613cfefe2c07f5a55de06d0e6b3d6b2a1
Quentin Vecchio <vecchioquentin@hotmail.fr>
2016-12-30 16:02:23 +0100

    Modification diverses

--- Sources/index.php
+++ Sources/index.php
@@ -15,12 +15,12 @@
 				</div>
 			</div>
 			<div id = "div_search_bar" class="row">
-				<div class="col-xs-9 col-sm-10 col-md-10 ">
+				<div class="col-md-6 col-md-offset-3">
 						<form>
 			        		<input id="search_bar" type="text" class="input-sm form-control" placeholder="Recherche">
 			       		</form>
 				</div>
-				<div class="col-xs-3 col-sm-2 col-md-2 ">
+				<div class="col-xs-2 col-sm- col-md-2 ">
 			       		<button id = "search_button" type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
 				</div>
 			</div>
@@ -30,5 +30,6 @@
 		<script src="js/jquery-ui.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/autocompletion.js"></script>
+		<script src="js/divin.js"></script>
 	</body>
 </html>
