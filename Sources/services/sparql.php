2bd18209ecc4ea2dfc0e1ee966e170c70c66cd37
Nicolas Chazeau <n.chazeau@laposte.net>
2016-12-30 16:44:06 +0100

    Modifs classes sparql

--- Sources/services/sparql.php
+++ Sources/services/sparql.php
@@ -1,53 +1,47 @@
 <?php
-require_once( "sparqllib.php" );
-
-$graphs = ['http://dbpedia.org', 'http://wineagent.tw.rpi.edu/data/wine.rdf'];
-$prefix_tab = ['foaf' => 'http://xmlns.com/foaf/0.1/',
-			   'dbo' => 'http://dbpedia.org/ontology/',
-			   'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#'];
-
-if(isset($_GET['query']) && $_GET['query'] != '') {
-	 
-	$db = sparql_connect( "http://dbpedia.org/sparql" );
-	if( !$db ) { print $db->errno() . ": " . $db->error(). "\n"; exit; }
-	foreach($prefix_tab as $key => $prefix) {
-		$db->ns($key, $prefix);
-	}
-
-	//EXEMPLE DE TEST
-	/*$sparql = "select * where {
-	{ <http://dbpedia.org/resource/Bordeaux> rdfs:label ?label. 
-          FILTER (langMatches (lang (?label) , \"en\")) }
-    UNION
-	{ <http://dbpedia.org/resource/Bordeaux> dbo:abstract ?text.
-          FILTER (langMatches (lang (?text) , \"en\"))}
-    UNION
-	{ <http://dbpedia.org/resource/Bordeaux> foaf:depiction ?photo.}
-    UNION
-        {<http://dbpedia.org/resource/Bordeaux> dbo:wikiPageExternalLink ?pages_liees.}
-	}	";*/
-
-	$sparql = "select * where {
-	{ ".$_GET['query']."> rdfs:label ?label. 
-          FILTER (langMatches (lang (?label) , \"en\")) }
-    UNION
-	{ ".$_GET['query']."> dbo:abstract ?text.
-          FILTER (langMatches (lang (?text) , \"en\"))}
-    UNION
-	{ ".$_GET['query']."> foaf:depiction ?photo.}
-    UNION
-        {".$_GET['query']."> dbo:wikiPageExternalLink ?pages_liees.}
-	}	";
-
-	$result = $db->query( $sparql ); 
-	if( !$result ) { print $db->errno() . ": " . $db->error(). "\n"; exit; }
-	 
-	$return = $result->fetch_all();
+	require_once( "sparqllib.php" );
 	
-	$json_return = json_encode($return);
-	echo '<pre>';
-	print_r($json_return);
-	echo '</pre>';
-	 
-
-}
+	public class Sparql {
+		private $graphs = ['http://dbpedia.org', 'http://wineagent.tw.rpi.edu/data/wine.rdf'];
+		private $prefix_tab = ['foaf' => 'http://xmlns.com/foaf/0.1/',
+							   'dbo' => 'http://dbpedia.org/ontology/',
+							   'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#'];
+		
+		private function connection() {
+			$db = sparql_connect( "http://dbpedia.org/sparql" );
+			if( !$db ) { print $db->errno() . ": " . $db->error(). "\n"; exit; }
+			foreach($prefix_tab as $key => $prefix) {
+				$db->ns($key, $prefix);
+			}
+		}
+		
+		private function performQuery($sparql) {
+			$result = $db->query( $sparql ); 
+			if( !$result ) { print $db->errno() . ": " . $db->error(). "\n"; exit; }
+			 
+			return $result->fetch_all();
+		}
+		
+		public function getDBPediaInfos($URI) {
+			$sparql = "select * where {
+						{ ".$URI."> rdfs:label ?label. 
+							  FILTER (langMatches (lang (?label) , \"en\")) }
+						UNION
+						{ ".$URI."> dbo:abstract ?text.
+							  FILTER (langMatches (lang (?text) , \"en\"))}
+						UNION
+						{ ".$URI."> foaf:depiction ?photo.}
+						UNION
+							{".$URI."> dbo:wikiPageExternalLink ?pages_liees.}
+						}	";
+						
+			return json_encode(performQuery($sparql));
+		}
+		
+		public function getWineRecettes($URI){
+			$sparql = "select * where {
+					      ?obj dbo:ingredient ".$URI.".
+					  }"
+			return json_encode(performQuery($sparql));
+		}
+	}
